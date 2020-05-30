b422e21606f51115685cae7263db777ebadd1e92
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-05-30 13:32:12 -0500

    fix api route path

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db');
 
-router.get('/api/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
-router.post('/api/notes', (req, res) => {
-    req.body.id = notes.length.toString();
-    notes.push(req.body)
-    res.json(req.body);
+router.post('/notes', (req, res) => {
+    const newNote = req.body
+    newNote.id = notes.length.toString();
+    notes.push(newNote)
+    res.json(newNote);
 });
 module.exports = router;
