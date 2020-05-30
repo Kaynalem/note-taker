dab86788837a854ff5a300798feed21c1d07ea78
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-05-30 16:56:07 -0500

    remove html route

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ app.use(express.json());
 app.use(express.static('public'));
 
 /*HTML routes*/
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
